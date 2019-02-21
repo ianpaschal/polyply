b06976908bf5f324270fa99440c0774280482e25
Ian <projects@ianpaschal.com>
2019-02-21 10:54:33 +0100

    Add core App class

---
+++ src/core/App.ts
@@ -0,0 +1,25 @@
+import Machine from "./Machine";
+
+class App {
+	_machines: Machine[];
+	_activeMachine: Machine;
+
+	set activeMachine( machine: Machine ) {
+		// Set the active machine to the given Machine instance
+		this._activeMachine = machine;
+		// If that machine hasn't been registered yet, make sure it is
+		this.addMachine( machine );
+	}
+
+	get activeMachineIndex() {
+		return this._machines.indexOf( this._activeMachine );
+	}
+
+	addMachine( machine: Machine ) {
+		if ( this._machines.indexOf( machine ) === -1 ) {
+			this._machines.push( machine );
+		}
+	}
+}
+
+export default App;
